105ed15c894213c40b6744a0062abe74d24bc0cf
shevakuilin <shevakuilin@gmail.com>
2021-02-18 18:30:45 +0800

    外部暴露抽象层提供固定参数 & 指定参数

--- SKDynamicLoader/SKDynamicLoader/Source/B.h
+++ SKDynamicLoader/SKDynamicLoader/Source/B.h
@@ -13,17 +13,39 @@
 /** 执行无返回值方法
  *
  * @param model 通信model
+ * @note  方法的参数固定为 responseData.data
  *
  */
 + (void)executeJSVoidMethodWithModel:(BridgeModel *)model;
 
+/** 执行无返回值方法
+ *
+ * @param model 通信model
+ * @param arguments  指定方法的参数
+ * @warning 注意 arguments 需要按照方法参数顺序传递，否则可能引发数据异常甚至崩溃
+ *
+ */
++ (void)executeJSVoidMethodWithModel:(BridgeModel *)model specifiedArguments:(NSArray *)arguments;
+
+
 /** 执行有返回值方法
  *
  * @param model 通信model
+ * @note  方法的参数固定为 responseData.data
  * @return 方法返回值
  *
  */
 + (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model;
+
+/** 执行有返回值方法
+ *
+ * @param model 通信model
+ * @param arguments  指定方法的参数
+ * @warning 注意 arguments 需要按照方法参数顺序传递，否则可能引发数据异常甚至崩溃
+ * @return 方法返回值
+ *
+ */
++ (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model specifiedArguments:(NSArray *)arguments;
 
 @end
 
--- SKDynamicLoader/SKDynamicLoader/Source/B.m
+++ SKDynamicLoader/SKDynamicLoader/Source/B.m
@@ -13,11 +13,34 @@
 
 @implementation B
 
-#pragma mark - 执行无返回值方法
+#pragma mark - Public
 
 + (void)executeJSVoidMethodWithModel:(BridgeModel *)model {
-    if (model) {
-        if (!kStringIsEmpty(model.eventName) && !kStringIsEmpty(model.moduleName) && model.responseData) {
+    [B executeJSVoidMethodWithModel:model arguments:nil];
+}
+
++ (void)executeJSVoidMethodWithModel:(BridgeModel *)model specifiedArguments:(NSArray *)arguments {
+    [B executeJSVoidMethodWithModel:model arguments:arguments];
+}
+
++ (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model {
+    return [B executeJSReturnValueMethodWithModel:model specifiedArguments:nil];
+}
+
++ (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model specifiedArguments:(NSArray *)arguments {
+    return [B executeJSReturnValueMethodWithModel:model specifiedArguments:arguments];
+}
+
+#pragma mark - Private
+
++ (void)executeJSVoidMethodWithModel:(BridgeModel *)model arguments:(NSArray *)arguments {
+    if (!model) { return; }
+    if (kStringIsEmpty(model.eventName) || kStringIsEmpty(model.moduleName)) { return; }
+    
+    if (arguments.count > 0) { // 指定参数
+        [A executeJSVoidMethod:model.eventName moduleName:model.moduleName arguments:arguments];
+    } else { // 固定参数
+        if (model.responseData) {
             if (model.responseData.data.allValues.count > 0) { // 方法带参数
                 [A executeJSVoidMethod:model.eventName moduleName:model.moduleName arguments:@[model.responseData.data]];
             } else { // 方法不带参数
@@ -27,11 +50,14 @@
     }
 }
 
-#pragma mark - 执行有返回值方法
-
-+ (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model {
-    if (model) {
-        if (!kStringIsEmpty(model.eventName) && !kStringIsEmpty(model.moduleName) && model.responseData) {
++ (id)executeJSReturnValueMethodWithModel:(BridgeModel *)model arguments:(NSArray *)arguments {
+    if (!model) { return nil; }
+    if (kStringIsEmpty(model.eventName) || kStringIsEmpty(model.moduleName)) { return nil; }
+    
+    if (arguments.count > 0) {  // 指定参数
+        return [A executeJSReturnValueMethod:model.eventName moduleName:model.moduleName arguments:arguments];
+    } else {
+        if (model.responseData) {
             if (model.responseData.data.allValues.count > 0) { // 方法带参数
                 return [A executeJSReturnValueMethod:model.eventName moduleName:model.moduleName arguments:@[model.responseData.data]];
             } else { // 方法不带参数
